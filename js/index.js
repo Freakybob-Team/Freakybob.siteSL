29b8079bf055777376f5911a165d928f96f10a38
mpax235 <millerpgraves@gmail.com>
2024-09-24 16:44:58 -0700

    add gpl license text to languages

--- js/index.js
+++ js/index.js
@@ -54,6 +54,7 @@ en: {
     activitiesButton: 'More activities!',
     popupText: 'Welcome to freakybob.site! Hope you enjoy the website just as much as we do!',
     thanksButton: 'Thanks!',
+    gplLicenseText: 'No Rights Reserved. All pages are licensed under GPL-3.',
 },
 es: {
     mainTitle: 'Freakybob',
@@ -66,6 +67,7 @@ es: {
     activitiesButton: '¡Mas actividades!',
     popupText: '¡Bienvenido a freakybob.site! ¡Esperamos que disfrutes el sitio web tanto como nosotros!',
     thanksButton: '¡Gracias!',
+    gplLicenseText: 'Sin derechos reservados. Todas las páginas están licenciadas bajo GPL-3.',
 },
 fr : {
     mainTitle: 'Freakybob',
@@ -78,6 +80,7 @@ fr : {
     activitiesButton: 'Dernières d\'activité!',
     popupText: 'Bienvenue sur freakybob.site! Nous sommes impatients de vous offrir le site Internet comme nous le faisons!',
     thanksButton: 'Merci!',
+    gplLicenseText: 'Aucun droit réservé. Toutes les pages sont sous licence GPL-3.',
 },
 de: {
     mainTitle: 'Freakybob',
@@ -90,6 +93,7 @@ de: {
     activitiesButton: 'Mehr Aktivität!',
     popupText: 'Willkommen auf freakybob.site! Wir hoffen, dass dir die Website genauso gefäch wie uns!',
     thanksButton: 'Danke!',
+    gplLicenseText: 'Keine Rechte vorbehalten. Alle Seiten sind unter GPL-3 lizenziert.',
 }
 ,jp : {
     mainTitle: 'Freakybob',
@@ -102,6 +106,7 @@ de: {
     activitiesButton: '活動を見てください',
     popupText: 'Freakybob.siteへようこそ! このウェブサイトを楽しんでいただきありがとうございます!',
     thanksButton: 'ありがとうございます!',
+    gplLicenseText: '無断転載禁止。すべてのページは GPL-3 ライセンスです。',
 }
 ,ko : {
     mainTitle: 'Freakybob',
@@ -114,6 +119,7 @@ de: {
     activitiesButton: '프레이티브박스를 확인해 보세요!',
     popupText: '프레이티브박스가 freakybob.site에 접근하고 어떤을!',
     thanksButton: '프레이티브박스를 확인해 보세요!',
+    gplLicenseText: '권리 없음. 모든 페이지는 GPL-3에 따라 라이센스됩니다.',
 }
 ,pt : {
     mainTitle: 'Freakybob',
@@ -126,6 +132,7 @@ de: {
     activitiesButton: 'Mais Atividade!',
     popupText: 'Bem-vindo ao freakybob.site! Esperamos que goste do site como eu fizemos!',
     thanksButton: 'Obrigado!',
+    gplLicenseText: 'Nenhum direito reservado. Todas as páginas estão licenciadas sob GPL-3.',
 }
 ,pl : {
     mainTitle: 'Freakybob',
@@ -137,6 +144,7 @@ de: {
     musicButton: 'Ogarnij muzyczę spongebob!',
     activitiesButton: 'Mehr Aktivitä!',
     thanksButton: 'Dziekuje!',
+    gplLicenseText: 'Brak Zastrzeżonych Praw. Wszystkie strony są licencjonowane na zasadach GPL-3.',
 }
 ,tr : {
     mainTitle: 'Freakybob',
@@ -149,6 +157,7 @@ de: {
     activitiesButton: 'Freakybob sayfasını deneyin!',
     popupText: 'Freakybob.site\'e hos geldiniz! Burada site size hızlıca çıkaracaktır!',
     thanksButton: 'Tesekkürler!',
+    gplLicenseText: 'Hiçbir Hakkı Saklı Değildir. Tüm sayfalar GPL-3 lisansı altındadır.',
 }
 ,th : {
     mainTitle: 'Freakybob',
@@ -161,6 +170,7 @@ de: {
     activitiesButton: 'ดูกิจกรรม!',
     popupText: 'ยินดีต้อนรับสู่ freakybob.site! ระบบของคุณจะใช้งานตอนนี้แล้ว!',
     thanksButton: 'ขอบคุณ!',
+    gplLicenseText: 'ไม่สงวนลิขสิทธิ์ ทุกหน้าได้รับอนุญาตภายใต้ GPL-3',
 }
 ,vi : {
     mainTitle: 'Freakybob',
@@ -173,6 +183,7 @@ de: {
     activitiesButton: 'Xem hoạt động!',
     popupText: 'Chào mừng đến freakybob.site! Chúng tôi mong muốn được ủng hộ trang web này!',
     thanksButton: 'Cảm ơn!',
+    gplLicenseText: 'Không có quyền được bảo lưu. Tất cả các trang được cấp phép theo GPL-3.',
 }
 ,uk : {
     mainTitle: 'Freakybob',
@@ -185,6 +196,7 @@ de: {
     activitiesButton: 'Інші активності!',
     popupText: 'Добро пожаловать на freakybob.site! Мы рады, что вам понравилось сайт как мы это делали!',
     thanksButton: 'Дякую!',
+    gplLicenseText: 'Права не захищені. Усі сторінки мають ліцензію GPL-3.',
 }
 ,zh : {
     mainTitle: 'Freakybob',
@@ -197,6 +209,7 @@ de: {
     activitiesButton: '更多活动!',
     popupText: '欢迎来到 freakybob.site! 我们很高兴为您服务!',
     thanksButton: '谢谢!',
+    gplLicenseText: '未保留任何权利。所有页面均根据 GPL-3 授权。',
 }
 ,ru : {
     mainTitle: 'Freakybob',
@@ -209,6 +222,7 @@ de: {
     activitiesButton: 'Ещё активности!',
     popupText: 'Добро пожаловать на freakybob.site! Мы рады, что вам понравилось сайт как мы это делали!',
     thanksButton: 'Спасибо!',
+    gplLicenseText: 'Права не защищены. Все страницы лицензированы по GPL-3.',
 }
 ,it : {
     mainTitle: 'Freakybob',
@@ -221,6 +235,7 @@ de: {
     activitiesButton: 'Altre attività!',
     popupText: 'Benvenuto su freakybob.site! Abbiamo apprezzato il sito come lo facciamo!',
     thanksButton: 'Grazie!',
+    gplLicenseText: 'Nessun diritto riservato. Tutte le pagine sono concesse in licenza GPL-3.',
 }
 ,lc : {
 mainTitle: 'Feakybob',
@@ -233,6 +248,7 @@ mainTitle: 'Feakybob',
     activitiesButton: 'More activities!',
     popupText: 'Welcome to freakybob.site! Hope you enjoy the website just as much as we do!',
     thanksButton: 'kthxbai',
+    gplLicenseText: 'No Rights Reserved. All pages are licensed under GPL-3.'
 }
 ,gr : {
     mainTitle: 'Gregreg',
@@ -245,6 +261,7 @@ mainTitle: 'Feakybob',
     activitiesButton: 'greg activities!',
     popupText: 'Welcome to gregybob.site! We hope you greg and enjoy greg as much as we do :WHAT~1:',
     thanksButton: 'greg!',
+    gplLicenseText: 'No Rights Reserved. All pages are licensed under GPL-3.'
 }
 };
     const languageSelector = document.getElementById('language-selector');
@@ -259,6 +276,7 @@ document.getElementById('activities-button').textContent = translations[lang].ac
 document.getElementById('marquee-text').textContent = translations[lang].marqueeText;
 document.getElementById('popup-text').textContent = translations[lang].popupText;
 document.getElementById('thanks-button').textContent = translations[lang].thanksButton;
+document.getElementById('gpl-3').textContent = translations[lang].gplLicenseText;
     }
 
     function setLanguage(lang) {
