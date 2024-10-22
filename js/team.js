191e150434d458718e9fc13525ebd0e71ff02b30
hacks.guide <128547338+Nomaakip@users.noreply.github.com>
2024-10-22 12:35:19 +0200

    oopsie spelling mistake

--- js/team.js
+++ js/team.js
@@ -5,7 +5,7 @@ function showInfo(name) {
         'Hater': '#2 Twitter Hater helped with giving all sorts of spongebob images!',
         'mpax':  'Mpax helps with the 3DS and IE6 versions! He also makes little tweaks to improve the site.',
         'names': 'Names helps with the Freakyblog, and also improves the sites!',
-        'Nomaakip': 'Helps fix the site and helped desing the lb website!',
+        'Nomaakip': 'Helps fix the site and helped desiging the lb website!',
     };
     const images = {
         'Squirrel': 'images/pfps/Boykisser Squirrel.png',
