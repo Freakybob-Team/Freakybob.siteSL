1d6dd771a8b2807b0a8842af2655985311b3cfe6
Squirrel <rylancarey2011@gmail.com>
2024-11-04 21:30:55 -0800

    Update christmasmusic.js

--- js/christmasmusic.js
+++ js/christmasmusic.js
@@ -5,7 +5,13 @@ const songs = [
     "https://ia800304.us.archive.org/7/items/300-christmas-songs_202305/Rockin%27%20Around%20The%20Christmas%20Tree%20-%20BRENDA%20LEE.mp3",
     "https://ia600304.us.archive.org/7/items/300-christmas-songs_202305/Run%20Rudolph%20Run%20-%20CHUCK%20BERRY.mp3",
     "https://ia800304.us.archive.org/7/items/300-christmas-songs_202305/White%20Christmas%20-%20BING%20CROSBY.mp3",
-    "https://ia800304.us.archive.org/7/items/300-christmas-songs_202305/Santa%20Baby%20-%20EARTHA%20KITT.mp3"
+    "https://ia800304.us.archive.org/7/items/300-christmas-songs_202305/Santa%20Baby%20-%20EARTHA%20KITT.mp3",
+    "https://ia804602.us.archive.org/4/items/wham-last-christmas-extended-version-of-original-45-mix-m-128s-re-creation/Wham%21%20-%20Last%20Christmas%20%28Extended%20Version%20of%20Original%2045%20Mix%29%20%5BM128%27s%20Re-creation%5D.mp3",
+    "https://ia600304.us.archive.org/7/items/300-christmas-songs_202305/Jingle%20Bell%20Rock%20-%20HALL%20%26%20OATES.mp3",
+    "https://ia800304.us.archive.org/7/items/300-christmas-songs_202305/Santa%20Claus%20is%20Coming%20to%20Town%20-%20BRUCE%20SPRINGSTEEN.mp3",
+    "https://ia800304.us.archive.org/7/items/300-christmas-songs_202305/Here%20Comes%20Santa%20Claus%20-%20DORIS%20DAY.mp3",
+    "https://ia800304.us.archive.org/7/items/300-christmas-songs_202305/Have%20Yourself%20a%20Merry%20Little%20Christmas%20-%20DAVID%20ARCHULETA%20%26%20CHARICE.mp3",
+    "https://github.com/Freakybob-Team/Freakybob.site/raw/refs/heads/main/audio/i-dont-gyatt-a-lot-for-chrizzmas-MARIAH-CAREY-ALL-I-GOT-FOR-CHRISTMAS-IS-YOU-PARODY.mp3"
 ];
 const songTitles = [
     "A Holly Jolly Christmas - Burl Ives",
@@ -14,9 +20,14 @@ const songTitles = [
     "Rockin' round The Christmas.. - Brenda Lee",
     "Run Rudolph Run - Chuck Berry",
     "White Christmas - Bing Crosby",
-    "Santa Baby - Eartha Kitt"
+    "Santa Baby - Eartha Kitt",
+    "Last Christmas - Wham!",
+    "Jingle Bell Rock - Hall & Oates",
+    "Santa is Coming to Town - Bruce Springsteen",
+    "Here Comes Santa Claus - Doris Day",
+    "Little Christmas - David A. & Charice",
+    "I Don't Gyatt a Lot for Chrizzmas - Mariah Carey"
 ];
-
 let currentSongIndex = 0;
 const audio = new Audio(songs[currentSongIndex]);
 audio.loop = true;
