58a9c0ca4a36bcbe990047fb3dbc11f6a98debc9
Squirrel <rylancarey2011@gmail.com>
2024-11-15 10:31:34 -0800

    Update christmasmusic.js

--- js/christmasmusic.js
+++ js/christmasmusic.js
@@ -22,6 +22,8 @@ const songs = [
     "https://ia804607.us.archive.org/3/items/rockin-around-the-christmas-tree/Brian%20Setzer%20Orchestra%20-%20Jingle%20Bells.mp3", //21
     "https://ia804607.us.archive.org/3/items/rockin-around-the-christmas-tree/George%20Thorogood%20%26%20The%20Destroyers%20%E2%80%93%20%E2%80%9CRock%20And%20Roll%20Christmas%E2%80%9D%20EMI%20America%201983.mp3", //22
     "https://archive.org/download/dababy2015murder-dababy2015murder1stchristmasspecial/02%20Lebron%20James%20%27Sprite%20Cranberry%27%20Commercial.mp3", // 23
+    // This is christmas music, trust
+    "https://github.com/5quirre1/in-the-end/raw/refs/heads/main/in-the-end.mp3", // 24
 ];
 
 const songTitles = [
@@ -47,7 +49,8 @@ const songTitles = [
     "Step Into Christmas - Elton John", //20
     "Jingle Bells (Rock) - Brian Setzer Orchestra", //21
     "Rock And Roll Christmas - George Thorogood & The Destroyers", //22
-    "Sprite Cranberry - Lebon bon",
+    "Sprite Cranberry - Lebon bon", /22
+    "This is Christmas music trust - Linkin Park", // 23
 ];
 
 
@@ -74,7 +77,7 @@ document.getElementById("play-pause-button").onclick = () => {
         document.getElementById("play-pause-button").textContent = "II";
     } else {
         audio.pause();
-        document.getElementById("play-pause-button").textContent = "▷II";
+        document.getElementById("play-pause-button").textContent = "II▷";
     }
 };
 
